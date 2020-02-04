93945b2ff39153256ec8d76aaa871ade10cedc35
Ethel-Ecc <ethelbert@ethel-consulting.com>
2020-02-04 19:46:16 +0100

    changed pos of navbar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,9 +49,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'bizstyle'
-html_theme_options = {
-    "rightsidebar": "false",
-}
+# html_theme_options = {
+#     "sidebarwidth": 12,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
