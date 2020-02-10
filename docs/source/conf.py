fe20b2b06f6dfa46a66503f5045c9a84f4845db9
Ethel-Ecc <ethelbert@ethel-consulting.com>
2020-02-10 21:54:02 +0100

    edited index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, EO'
 author = 'EO'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
